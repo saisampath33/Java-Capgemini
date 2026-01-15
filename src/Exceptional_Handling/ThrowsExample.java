33baec967cd419a693d22c18742038a50c536e22
saisampath33 <sampathmay10@gmail.com>
2026-01-15 16:19:23 +0530

    Started Exceptional_Handling

---
+++ src/Exceptional_Handling/ThrowsExample.java
@@ -0,0 +1,20 @@
+package Exceptional_Handling;
+
+public class ThrowsExample {
+	public static void m1() throws ArithmeticException{
+		int a=10;
+		int b=0;
+		System.out.println(a/b);
+	}
+	public static void m2() {
+		try {
+			m1();
+		}catch(Exception e) {
+			System.out.println("Caught");
+		}
+	}
+	public static void main(String[] args) {
+		m2();
+	}
+
+}
