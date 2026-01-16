eea5e240ed3cd9fa7a7692b41277a815525e2a75
saisampath33 <sampathmay10@gmail.com>
2026-01-16 11:31:44 +0530

    Started with wrapper classes

---
+++ src/Wrapper_Classes/Boxing.java
@@ -0,0 +1,38 @@
+package Wrapper_Classes;
+
+public class Boxing {
+
+	public static void main(String[] args) {
+		//boxing
+		
+		int a = 10;
+		Integer z = new Integer(a); //(OLD / Deprecated
+		System.out.println(z);
+		
+		int b = 20;
+		Integer i = Integer.valueOf(b);
+		System.out.println(i);
+		
+		//Auto Boxing
+		int x = 30;
+		Integer y = x;
+		System.out.println(y);
+		
+		//Unboxing
+		int u = y.intValue();
+		System.out.println(u);
+		
+		//boxing and unboxing
+		byte x1 = 50;
+		Byte y1 = Byte.valueOf(x1); //box
+		byte z1 = y1.byteValue(); //un box
+		System.out.println(z1);
+		
+		//auto unboxing
+		int var = 77;
+		Integer d1 = var;
+		int f1 = d1;
+		System.out.println(f1);
+	}
+
+}
