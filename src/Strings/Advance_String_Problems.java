bb28b68019ba4cad77517f64404544f095f63a30
saisampath33 <sampathmay10@gmail.com>
2026-01-17 10:08:57 +0530

    Wrapper Class

---
+++ src/Strings/Advance_String_Problems.java
@@ -0,0 +1,32 @@
+package Strings;
+
+public class Advance_String_Problems {
+	public static boolean string_contains_digits(String s) {
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(i)<'0' || s.charAt(i)>'9') {
+				return false;
+			}
+		}
+		return true;
+	}
+	public static boolean string_contains_digits_builtin(String s) {
+		for(char c : s.toCharArray()) {
+			if(!Character.isDigit(c)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	public static String removeSpecial(String s) {
+		return s.replaceAll("[^a-zA-Z0-9]", s);
+	}
+	public static void main(String[] args) {
+		
+		String s ="123";
+		System.out.println("string_contains_digits: "+string_contains_digits(s));
+		System.out.println("string_contains_digits_builtin: "+string_contains_digits(s));
+		System.out.println(removeSpecial("Cap@2026#India!"));
+
+	}
+
+}
