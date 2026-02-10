57c07b74312095d3769b823e047a80fdba39b394
saisampath33 <sampathmay10@gmail.com>
2026-02-10 21:46:16 +0530

    modified

--- src/Collections/Maps.java
+++ src/Collections/Maps.java
@@ -1,11 +1,15 @@
 package Collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Maps {
 
 	public static void main(String[] args) {
-		HashMap map = new HashMap();
+		HashMap<Integer,String> map = new HashMap<>();
 		map.put(1, "Sampath");
 		map.put(16, "Varun");
 		map.put(34, "Manikanta");
@@ -25,6 +29,14 @@ public class Maps {
 		
 		for(Object o : map.keySet()) {
 			System.out.print(o+" ");
+		}
+		Map<String,Integer> mpp = new HashMap<>();
+		ArrayList<String> arr = new ArrayList<>(Arrays.asList("sai","sai","sam","varun","vishnu"));
+		for(String ar:arr) {
+			mpp.put(ar,mpp.getOrDefault(ar, 0)+1);
+		}
+		for(Map.Entry<String,Integer> entry: mpp.entrySet()) {
+			System.out.println("key-> "+entry.getKey()+" Value-> "+entry.getValue());
 		}
 	}
 
